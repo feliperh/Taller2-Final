5c493688cff2fc6b70f215c3e0e277b457605917
Andres Romero <andresromero@Andress-MacBook-Pro.local>
2017-05-04 15:20:50 -0500

    Entrega1

---
+++ src/main/java/com/edu/sergioarboleda/DISPENSADOR/App.java
@@ -0,0 +1,13 @@
+package com.edu.sergioarboleda.DISPENSADOR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
